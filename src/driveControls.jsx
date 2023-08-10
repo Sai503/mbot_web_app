6e96100f7d47e3bb294bc78cb5f7f477f73c728e
imadhav <imadhav@umich.edu>
2023-08-10 15:37:01 -0400

    removed extras

--- src/driveControls.jsx
+++ src/driveControls.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  faArrowUp,
-  faArrowDown,
-  faArrowLeft,
-  faArrowRight,
   faArrowRotateLeft,
   faArrowRotateRight
 } from '@fortawesome/free-solid-svg-icons'
