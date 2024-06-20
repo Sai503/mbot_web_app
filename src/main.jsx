9e8a5ee598dde09056f46906f1ec23cf4bf8c826
Jana Pavlasek <jbpavlasek@gmail.com>
2024-06-20 17:23:23 -0400

    Use then instead of top-level await which works in production

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import MBotApp from './app'
 import { MBotScene } from './scene.js'
 
 const scene = new MBotScene();
-await scene.init();
-
-ReactDOM.render(
-  <MBotApp scene = {scene}/>,
-  document.getElementById('app-root')
-);
+scene.init()
+  .then(() => {
+    ReactDOM.render(
+      <MBotApp scene = {scene}/>,
+      document.getElementById('app-root')
+    );
+  });
